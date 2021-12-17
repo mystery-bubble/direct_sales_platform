7228b18dea347955fc2fe6ae6d9ff9cead7b862d
kingdom84521 <kingdomhwang@gmail.com>
2021-12-17 23:13:30 +0800

    [NEW/frontend]: set cart item to sell functioned

--- @frontend/src/components/unique/Cart/x-cart-item.vue
+++ @frontend/src/components/unique/Cart/x-cart-item.vue
@@ -1,38 +1,39 @@
 <template>
-  <div class="cart-item-container">
+  <div
+    class="cart-item-container"
+    :class="{ 'selected': isChecked }"
+    @click="changeHandler( !isChecked )"
+  >
     <div class="c-item action">
-      <input type="checkbox">
+      <input type="checkbox" :checked="isChecked" @change="changeHandler( $event.target.checked )">
     </div>
     <div class="c-item picture">
-      <img class="item-picture" src="https://via.placeholder.com/500">
+      <img class="item-picture" :src="info.imgsrc">
     </div>
-    <div class="c-item shrink-content">test</div>
+    <div class="c-item shrink-content">
+      test
+    </div>
     <div class="c-item title">
-      <div>
-        這是一個超讚的商品這是一個超讚的商品這是一個超讚的商品這是一個超讚的商品這是一個超讚的商品這是一個超讚的商品這是一個超讚的商品
-      </div>
+      {{ info.title }}
     </div>
-    <div class="c-item specs">
-      <select name="specs" class="border border-custom-black">
+    <div class="c-item types">
+      <select name="specs" class="border border-custom-black" @change="optionChangeHandler( $event.target.value )" @click.stop="selectMask">
         <option disabled selected hidden>選擇規格</option>
-        <option value="">testsetsetset</option>
-        <option value="">testsetsetset</option>
-        <option value="">testsetsetset</option>
-        <option value="">testsetsetset</option>
+        <option v-for="( type, index ) in info.types" :key="`type-${ index }`" :value="type.id">{{ type.name }}</option>
       </select>
     </div>
-    <div class="c-item price">$ 9999999</div>
+    <div class="c-item price">$ {{ info.price }}</div>
     <div class="c-item amount">
-      <div class="sign">
-        <Icon icon="mdi:plus" />
-      </div>
-      <div class="number">99999</div>
-      <div class="sign">
+      <div class="sign" @click.stop="--info.amount">
         <Icon icon="mdi:minus" />
       </div>
+      <div class="number">{{ info.amount }}</div>
+      <div class="sign" @click.stop="++info.amount">
+        <Icon icon="mdi:plus" />
+      </div>
     </div>
-    <div class="c-item subtotal">$ 999999</div>
-    <div class="c-item check-sign">
+    <div class="c-item subtotal">$ {{ this.subtotal }}</div>
+    <div class="c-item check-sign" v-show="isChecked">
       <svg
         class="ml-1.5"
         xmlns="http://www.w3.org/2000/svg"
@@ -55,9 +56,50 @@
 
 <script>
 export default {
+  data: () => ({
+    info: {
+      title: "這是一個超讚的商品這是一個超讚的商品這是一個超讚的商品這是一個超讚的商品這是一個超讚的商品這是一個超讚的商品這是一個超讚的商品",
+      imgsrc: "https://via.placeholder.com/500",
+      types: [
+        {
+          id: "00000000",
+          name: "選項 1"
+        },
+        {
+          id: "00000001",
+          name: "選項 2"
+        },
+        {
+          id: "00000002",
+          name: "選項 3"
+        }
+      ],
+      price: 5000,
+      amount: 1000
+    },
+    isChecked: false,
+    selectedOption: -1
+  }),
   methods: {
     changeHandler( state ) {
+      this.isChecked = state
       this.$emit( "change", state )
+    },
+    selectMask() {
+      return true
+    },
+    optionChangeHandler( id ) {
+      for ( let iter in this.info.types ) {
+        if ( this.info.types[ iter ].id === id ) {
+          this.selectedOption = iter
+          return
+        }
+      }
+    }
+  },
+  computed: {
+    subtotal() {
+      return this.info.amount * this.info.price
     }
   }
 }
