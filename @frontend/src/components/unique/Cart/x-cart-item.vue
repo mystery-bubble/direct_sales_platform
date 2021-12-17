2b6e7360978ac76461259081e2a4aa6df794180e
kingdom84521 <kingdomhwang@gmail.com>
2021-12-17 17:33:01 +0800

    [NEW/frontend]: cart item base structure done.

---
+++ @frontend/src/components/unique/Cart/x-cart-item.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="cart-item-container">
+    <div class="c-item action">
+      <input type="checkbox">
+    </div>
+    <div class="c-item picture">
+      <img class="item-picture" src="https://via.placeholder.com/500">
+    </div>
+    <div class="c-item shrink-content">test</div>
+    <div class="c-item title">
+      <div>
+        這是一個超讚的商品這是一個超讚的商品這是一個超讚的商品這是一個超讚的商品這是一個超讚的商品這是一個超讚的商品這是一個超讚的商品
+      </div>
+    </div>
+    <div class="c-item specs">
+      <select name="specs" class="border border-custom-black">
+        <option disabled selected hidden>選擇規格</option>
+        <option value="">testsetsetset</option>
+        <option value="">testsetsetset</option>
+        <option value="">testsetsetset</option>
+        <option value="">testsetsetset</option>
+      </select>
+    </div>
+    <div class="c-item price">$ 9999999</div>
+    <div class="c-item amount">
+      <div class="sign">
+        <Icon icon="mdi:plus" />
+      </div>
+      <div class="number">99999</div>
+      <div class="sign">
+        <Icon icon="mdi:minus" />
+      </div>
+    </div>
+    <div class="c-item subtotal">$ 999999</div>
+    <div class="c-item check-sign">
+      <svg
+        class="ml-1.5"
+        xmlns="http://www.w3.org/2000/svg"
+        width="14"
+        height="14"
+        viewBox="0 0 18.101 18.544"
+      >
+        <path 
+          d="M1509.117,18.5l4.165,5.863,11.49-16.175"
+          transform="translate(-1507.894 -7.319)"
+          fill="none"
+          stroke="#f3f4f5"
+          stroke-linejoin="round"
+          stroke-width="3"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    changeHandler( state ) {
+      this.$emit( "change", state )
+    }
+  }
+}
+</script>
+
+<style>
+@import "../../../styles/components/unique/Cart/x-cart-item.pcss";
+</style>
---
+++ @frontend/src/views/Cart.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="view cart">
+    <div class="cart main-part">
+      <div class="main head">
+        <div class="inline-block w-1/2">商品</div>
+        <div class="inline-block w-1/6">單價</div>
+        <div class="inline-block w-1/6">數量</div>
+        <div class="inline-block w-1/6">小計</div>
+      </div>
+      <span class="main-container">
+        <x-cart-item></x-cart-item>
+      </span>
+    </div>
+    <div class="cart event-part"></div>
+    <div class="cart action-part"></div>
+  </div>
+</template>
+
+<script>
+import XCartItem from "@/components/unique/Cart/x-cart-item.vue"
+
+export default {
+  components: {
+    XCartItem
+  },
+  methods: {
+    test( val ) {
+      console.log( val )
+    }
+  }
+}
+</script>
+
+<style scoped lang="postcss">
+@import "../styles/views/Cart.pcss";
+</style>
