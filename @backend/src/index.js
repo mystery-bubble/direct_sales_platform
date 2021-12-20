811f74c25442d7fbc00c57c190a4ce0bbf4c5574
kingdom84521 <kingdomhwang@gmail.com>
2021-12-20 09:19:15 +0800

    [INIT/backend]: some initial file copy from old project.

--- @backend/src/index.js
+++ @backend/src/index.js
@@ -1,1 +1,41 @@
-console.log("just for initialization")
+const express = require( "express" );
+const cors = require( "cors" )
+
+const config = require( "./config" );
+const customResponses = require( "./middlewares/customResponses" );
+// const logger = require( "./utilities/logger" );
+
+const app = express();
+const port = process.env.PORT || config.port;
+const ENV = process.env.NODE_ENV || config.env;
+
+app.set( "env", ENV );
+
+app.use( express.json() );
+app.use( cors() )
+app.use( customResponses );
+
+require( "./config/mongoose" )( app );
+require( "./app" )( app );
+
+app.use( ( req, res ) => {
+    res.notFound();
+} );
+
+app.use( ( err, req, res, next ) => {
+    // logger.error( err.stack );
+    next( err );
+} );
+
+// Don't remove next !!!!
+app.use( ( err, req, res, next ) => { // eslint-disable-line no-unused-vars
+    res.status( 503 ).json( {
+        success: false,
+        error: "server_error",
+    } );
+} );
+
+app.listen( port, () => {
+    // logger.info( `Listening on port ${ port }` );
+    console.log( `Listening on port ${ port }` );
+} );
