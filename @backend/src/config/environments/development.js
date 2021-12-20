bc47f565ee81650bde0d9f2024d6f4ea1877da53
kingdom84521 <kingdomhwang@gmail.com>
2021-12-20 18:33:31 +0800

    [FIX/backend]: remove secrets

--- @backend/src/config/environments/development.js
+++ @backend/src/config/environments/development.js
@@ -1,8 +1,8 @@
 module.exports = {
     env: "development",
-    host: "120.0.0.1",
+    host: "",
     port: 1234,
-    mongoUrl: "mongodb://lab305:E2hGB4M2UE@120.105.129.33:27017/food-delivery",
+    mongoUrl: "",
     logLevel: "debug",
     secret: "ultimateSecretZone" 
 }
