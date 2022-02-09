464fd8fd39be0e9a28875a67020bc6f0fedf6745
kingdom84521 <kingdomhwang@gmail.com>
2022-02-09 18:09:35 +0800

    [NEW/config]: add nginx.conf for vue-router

--- @backend/src/index.js
+++ @backend/src/index.js
@@ -1,6 +1,6 @@
 const express = require( "express" );
-const cors = require( "cors" )
-const helmet = require("helmet")
+const cors = require( "cors" );
+const helmet = require( "helmet" );
 
 const config = require( "./config" );
 const customResponses = require( "./middlewares/customResponses" );
@@ -12,7 +12,7 @@ const ENV = process.env.NODE_ENV || config.env;
 
 app.set( "env", ENV );
 
-app
+app.use( helmet() )
 app.use( express.json() );
 app.use( express.urlencoded( { extended: true } ) );
 app.use( cors( config.corsSettings ) )
